52b23d23d7a8db79c4a67da6c15e4bcd512be4b4
Christian <chris6por@gmail.com>
2018-07-09 14:50:29 +0000

    installed and connected redux devtools to application and used spread operator to set payload data in store

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -3,8 +3,11 @@ import { FETCH_COURSES } from '../actions/types';
 export default function(state = [], actions) {
   switch (actions.type) {
     case FETCH_COURSES:
-        console.log(actions.payload);
-        return state;
+        
+        return [
+          ...state,
+          ...actions.payload
+        ];
       default:
         return state;
   }
